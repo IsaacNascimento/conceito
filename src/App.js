e1939bcab81a6558dff915f06d5dadcd7c123941
IsaacNascimento <isaacs.nascimento17@gmail.com.com>
2022-03-23 21:47:54 -0300

    props and components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Menu from "./components/Menu";
+import Cartao from "./components/Cartao";
+
+
+
+
 
 function App() {
-  return  (
-     <body>
-       <h1>Hello World</h1>
+  return (
+    <body>
+
       <Menu/>
+
+      <Cartao tittle="É o trikas" image="https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg"/>
+
+      <br>
+      </br>
+
+      <h1>Hello World</h1>
+     
       <Button variant="primary">Primary</Button>
-     </body>
+    </body>
   );
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Menu = () => {
   return (
     <div >
-      <ul>
-        <li>
-          <a href="">Pagina1</a>
-        </li>
-        <li>
-          <a href="">Pagina2</a>
-        </li>
-        <li>
-          <a href="">Pagina2</a>
-        </li>
-      </ul>
+      <Navbar bg="dark" variant="dark" className="mb-5">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
